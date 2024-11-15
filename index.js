49e129b23d752756c5d8c27a86475669239f6860
facundolizardo75@gmail.com <facundolizardo75@gmail.com>
2024-11-15 12:56:54 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const dotenv = require("dotenv");
+const app = express();
+const port = 3000;
+
+// Middleware para parsear JSON en las solicitudes
+app.use(express.json());
+
+async function buscarProductos(req, res) {
+    const { query } = req.body;
+    res.json({ query, products: ["pene"] });
+}
+
+// Ruta principal
+app.post('/buscarProductos', buscarProductos);
+
+// Inicia el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:3000`);
+});
