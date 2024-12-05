6af263bb1b049ed6520665db7cede6df14f9ecb5
facundolizardo75@gmail.com <facundolizardo75@gmail.com>
2024-12-05 18:06:07 -0300

    fixes enviar email

--- routes/enviarEmail.js
+++ routes/enviarEmail.js
@@ -27,52 +27,112 @@ router.post("/", async (req, res) => {
     nombreSucursal,
     apiKey,
   } = req.body;
-
   const api_key = process.env.API_KEY;
-
   try {
-    if (!apiKey || api_key !== apiKey) {
-      return res.status(401).json({ message: "Api key no proporcionada o incorrecta" });
+    if (!apiKey) {
+      return res.status(401).json({ message: "Api key no proporcionada" });
     }
-
+    if (api_key !== apiKey) {
+      return res.status(401).json({ message: "Api key incorrecta", apiKey });
+    }
     if (!nombre || !documento || !tipoDeDocumento || !productos || !entrega) {
       return res.status(400).json({
-        message: "Campos obligatorios faltantes",
+        message:
+          "Los campos nombre, documento, tipoDeDocumento, productos y entrega son obligatorios",
       });
     }
-
+    // Validar campos según el tipo de entrega
     if (entrega === "1" && (!codigoSucursal || !nombreSucursal)) {
       return res.status(400).json({
-        message: "Campos obligatorios para entrega en sucursal faltantes",
+        message:
+          "Los campos codigoSucursal y nombreSucursal son obligatorios para el tipo de entrega 2",
       });
     }
-
+    if (entrega === "2" && !direccion) {
+      return res.status(400).json({
+        message: "El campo direccion es obligatorio para el tipo de entrega 1",
+      });
+    }
+    // Limpiar el string de productos eliminando escapes adicionales
     let arrayProductos;
     try {
-      arrayProductos = JSON.parse(productos.replace(/\\\\/g, "\\"));
+      const cleanedProductos = productos.replace(/\\\\/g, "\\"); // Elimina \\ adicionales
+      arrayProductos = JSON.parse(cleanedProductos); // Parsear el string limpio
     } catch (error) {
       return res.status(400).json({
-        message: "Productos no tienen un formato válido",
+        message: "El campo productos no tiene un formato JSON válido",
+        error: error.message,
       });
     }
-
-    let coordenadas = null;
-    if (entrega === "2") {
-      coordenadas = await obtenerCoordenadas(direccion, "Paraguay");
+    if (!Array.isArray(arrayProductos)) {
+      return res
+        .status(400)
+        .json({ message: "El campo productos debe ser un array válido" });
     }
-
+    let coordenadas = null;
+    try {
+      if (entrega === "2") {
+        coordenadas = await obtenerCoordenadas(direccion, "Paraguay");
+      }
+    } catch (error) {
+      if (
+        error.message ===
+        "No se encontraron resultados para la dirección proporcionada."
+      ) {
+        return res.status(200).json({ message: error.message, error });
+      }
+      throw error;
+    }
     const mailOptions = {
       from: "facundolizardotrabajosia@gmail.com",
-      to: "facundolizardo75@gmail.com",
+      to: "facundolizardo75@gmail.com,victor.barreto@bristol.com.py",
       subject: "Correo de test del agente de ventas",
-      text: JSON.stringify(arrayProductos, null, 2),
-    };
-
-    await transporter.sendMail(mailOptions);
-    res.status(200).json({ message: "Correo enviado con éxito" });
-  } catch (error) {
-    console.log(error);
+      text: `Hola, este es un correo enviado para testear el agente de ventas.
+      Informacion de la compra: 
+      - Cliente
+      Nombre: ${nombre}
+      tipoDeDocumento: ${tipoDeDocumento} 
+      documento: ${documento} 
+      - Productos
+      ${arrayProductos
+        .map((producto, index) => {
+          const number = index + 1;
+          return `
+      ${number}. Nombre: ${producto.nombre}
+        codigobarra: ${producto.codigobarra} 
+        Cantidad: ${producto.cantidad}
+        Precio: ${producto.precio.total}
+        Condicion: ${producto.precio.condicion}
+        Cuotas: ${producto.precio.cuotas}
+        Monto de la cuota: ${producto.precio.monto}
+        precio id: ${producto.precio.id}
+      `;
+        })
+        .join("")}
     
+      - Datos de entrega o retiro
+      Tipo de entrega: ${entrega}
+      ${
+        entrega === "1"
+          ? `Sucursal: 
+       * Codigo de sucursal: ${codigoSucursal}
+       * Nombre de sucursal: ${nombreSucursal}`
+          : `Direccion del cliente: ${direccion}, Paraguay
+      Ubicacion:
+       * Latitud: ${coordenadas.lat}
+       * Longitud: ${coordenadas.lon}`
+      }
+      `,
+    };
+    // Enviar correo
+    const info = await transporter.sendMail(mailOptions);
+    res.status(200).json({ message: "Correo enviado con éxito", info });
+  } catch (error) {
+    if (
+      error === "No se encontraron resultados para la dirección proporcionada."
+    ) {
+      res.status(500).json({ message: error });
+    }
     res.status(500).json({ message: "Error al enviar el correo", error });
   }
 });
